94536e98bd19f586e233b95e77a9627a037153a9
Abdou BA <ba2664890@gmail.com>
2025-11-09 17:01:09 +0000

    troisiéme commitater

--- src/pages/AdminRedirect.tsx
+++ src/pages/AdminRedirect.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const AdminRedirect = () => {
   useEffect(() => {
-    window.location.href = "http://localhost:8000/admin/";
+    window.location.href = "https://backendsante-production.up.railway.app/admin";
   }, []);
 
   return <div className="text-center mt-20 text-lg">Redirection vers l'admin Django...</div>;
