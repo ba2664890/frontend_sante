de4ff023ea6a731442686395ee1b930a7096d22b
Abdou BA <ba2664890@gmail.com>
2025-11-21 09:03:12 +0000

    modification de la carte

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ const App: React.FC = () => (
       <Routes>
         {/* public route */}
         <Route path="/login" element={<HomePage />} />
+        <Route path="/registre" element={<Login />} />
 
         
 
