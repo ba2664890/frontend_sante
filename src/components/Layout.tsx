a79e2d0126f63d0c5b5b496f88fb5f8272075195
Abdou BA <ba2664890@gamil.com>
2026-04-28 20:06:44 -0400

    refonte du design

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,15 +6,12 @@ import bgImage from '../assets/background.jpg';
 
 const Layout: React.FC = () => {
   return (
-    <div
-      className="min-h-screen bg-cover  bg-no-repeat flex"
-      style={{ backgroundImage: `url(${bgImage})` }}
-    >
+    <div className="min-h-screen bg-background flex overflow-x-hidden">
       <Sidebar />
-      <div className="flex-1 flex flex-col bg-white/70 backdrop-blur-sm">
+      <div className="flex-1 flex flex-col min-w-0">
         <Header />
-        <main className="flex-1 p-6">
-          <div className="max-w-7xl mx-auto">
+        <main className="flex-1 overflow-y-auto">
+          <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
             <Outlet />
           </div>
         </main>
